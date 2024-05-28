aba8cc86e3a74ad35fcff27642ac3e10e34f6ef2
Dominik Sauerer <dominik.sauerer@gmail.com>
2024-05-28 14:30:01 +0200

    Init spring boot project

---
+++ src/main/java/fau/fdm/OntoFormGenerator/OntoFormGeneratorApplication.java
@@ -0,0 +1,13 @@
+package fau.fdm.OntoFormGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntoFormGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntoFormGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fau/fdm/OntoFormGenerator/OntoFormGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package fau.fdm.OntoFormGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntoFormGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
