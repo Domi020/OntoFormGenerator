84591625b724ad3c706376ad85422f14b364701f
Dominik Sauerer <dominik.sauerer@gmail.com>
2024-06-07 15:55:39 +0200

    Init form editor

--- src/main/java/fau/fdm/OntoFormGenerator/controller/RestController.java
+++ src/main/java/fau/fdm/OntoFormGenerator/controller/RestController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,5 +26,11 @@ public class RestController {
         model.addAttribute("ontologies", ontologyOverviewService.getNamesOfImportedOntologies());
 
         return "index";
+    }
+
+    @RequestMapping(value = "/editor/{ontology}", method = RequestMethod.GET)
+    public String loadEditorPage(Model model, @PathVariable String ontology) {
+        model.addAttribute("ontology", ontology);
+        return "editor";
     }
 }
